cebb6ee4909aa5a784800b0d8efe2b7b04559b51
vannama <vannama@ncsu.edu>
2017-11-11 18:10:49 -0500

    Modified Network.py

--- pythonCode/Network.py
+++ pythonCode/Network.py
@@ -1,60 +1,72 @@
 from Node import Node
 import time
+import collections
 
 class Network:
-	def __init__(self,slot_time,max_time):
-		self.tp=10 #microsec
+	def __init__(self,slot_time,max_time,nodeCount,distanceBetweenNodes):
 		self.slot_time=slot_time
 		self.cur_time=0
 		self.tt=80
 		self.max_time=max_time
 		self.collCount=0
 		self.bandwidth = 100 #Mbps
+		self.vel=2*(10**8)
+		self.nodeCount=nodeCount
+		self.distanceBetweenNodes=distanceBetweenNodes
+		self.distance=collections.defaultdict(int)
+		self.node=[]
+		for i in range(1,self.nodeCount+1):
+			self.node[i]=Node(i,l/self.slot_time)
+			self.distance[i]=(i-1)*self.distanceBetweenNodes
 	
-	def run(self,node1,node2):
-		self.packet_transmit(node1,node2)
+	def run(self):
+		for i in range(1,self.nodeCount+1):
+			self.packet_transmit(self.node[i])
 		self.cur_time = self.cur_time + 1
 
-	def packet_transmit(self,node1,node2):
+	def packet_transmit(self,node):
 		#decide the node to be transmitted
 		#check backoff time for that node
 		#if backoff time for any node is 0, then 
 		#call transmit for that node
-		node1.operation(self)
-		node2.operation(self)
-		self.coll_detect(node1, node2)
+		node.operation(self)
+		self.coll_detect()
 
 		print(self.cur_time)
-		print("Status of Node 1: ", node1.status)
-		print("Status of Node 2: ", node2.status)
-		print("-------------------------------------")
-		print("-------------------------------------")
+		for i in range(1,self.nodeCount+1):
+			print("Status of Node {}: {}".format(i,self.node[i].status))
+			print("-------------------------------------")
 		# return self
 		
-	def coll_detect(self,node1,node2):
-		if node1.status=="Transmitting" and node2.status=="Transmitting":
+	def coll_detect(self):
+		tempCount=0
+		collIndex=[]
+		for i in range(1,self.nodeCount+1):
+			if node[i].status=="Transmitting":
+				tempCount+=1
+				collIndex.append(i)
+		if tempCount>=2:		
 			self.collCount=self.collCount+1
-			node1.stopTransmit("Collision")
-			node2.stopTransmit("Collision")
+			for i in collIndex:
+				node[i].stopTransmit("Collision")
 	
-	def print_stat(self, node1, node2):
-		print("Total packets sent from A: ", node1.packetCount-1)
-		print("Total packets sent from B: ", node2.packetCount-1)
+	def print_stat(self):
+		for i in range(1,nodeCount+1):
+			print("Total packets sent from Node {}: {}".format(i,node[i].packetCount-1))
+			print("Average end to end throughput from Node {}: {}".format(node[i].throughput(self)))
 		print("Number of collisions: ", self.collCount)
-		print("Average end to end throughput from A to B: ", node1.throughput(self))
-		print("Average end to end throughput from B to A: ", node2.throughput(self))
 		print("Simulation end time", self.cur_time)
 	
 if __name__ == "__main__":
 	# slot_time=int(input("Enter the slot time"))
 	slot_time = 50
 	l = 0.5
+	distanceBetweenNodes=2000
 	max_time =int(input("Enter the max time: "))
-	part1=Network(slot_time,max_time)
-	node1=Node(1,l/slot_time)
-	node2=Node(2,l/slot_time)
+	nodeCount=int(input("Enter the number of nodes: "))
+	part2=Network(slot_time,max_time,nodeCount,distanceBetweenNodes)	
 	for _ in range(max_time+1):
-		part1.run(node1,node2)
+		part2.run()
 		# time.sleep(1)	
 
-	part1.print_stat(node1, node2)
+	part2.print_stat()
