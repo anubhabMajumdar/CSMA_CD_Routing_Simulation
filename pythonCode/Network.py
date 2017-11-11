8071930743033fccd6f1725ef32a5e8cc61c0568
vannama <vannama@ncsu.edu>
2017-11-11 18:30:23 -0500

    Modified constructor to include lambda

--- pythonCode/Network.py
+++ pythonCode/Network.py
@@ -3,7 +3,7 @@ import time
 import collections
 
 class Network:
-	def __init__(self,slot_time,max_time,nodeCount,distanceBetweenNodes):
+	def __init__(self,l,slot_time,max_time,nodeCount,distanceBetweenNodes):
 		self.slot_time=slot_time
 		self.cur_time=0
 		self.tt=80
@@ -15,8 +15,9 @@ class Network:
 		self.distanceBetweenNodes=distanceBetweenNodes
 		self.distance=collections.defaultdict(int)
 		self.node=[-1]
+		self.lambd=l
 		for i in range(1,self.nodeCount+1):
-			self.node.append(Node(i,l/self.slot_time))
+			self.node.append(Node(i,float(self.lambd)/self.slot_time))
 			self.distance[i]=(i-1)*self.distanceBetweenNodes
 	
 	def run(self):
@@ -57,7 +58,7 @@ if __name__ == "__main__":
 	distanceBetweenNodes=2000
 	max_time =int(input("Enter the max time: "))
 	nodeCount=int(input("Enter the number of nodes: "))
-	part2=Network(slot_time,max_time,nodeCount,distanceBetweenNodes)	
+	part2=Network(l,slot_time,max_time,nodeCount,distanceBetweenNodes)	
 	for _ in range(max_time+1):
 		part2.run()
 		time.sleep(1)	
