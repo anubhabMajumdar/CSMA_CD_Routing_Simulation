32165ed21a47e34c7730e8c963e148203c76f5b9
vannama <vannama@ncsu.edu>
2017-11-25 13:52:11 -0500

    Basic Router changes

--- pythonCode/Router.py
+++ pythonCode/Router.py
@@ -2,16 +2,16 @@ import Packet
 class Router:
 	def __init__(self,id):
 		self.id=id
-		self.route_table=[]
+		self.route_table={}
 		self.buffer=[]
-		
+
 	def add_packet(self,packet):
 		self.buffer.append(packet)
 
 	def update_table(self):
 
 	def routing(self,packet):
-		packet.IP=self.route_table(packet.IP) 
+		packet.mac=self.route_table(packet.IP) 
 
 	def operation(self):
 
