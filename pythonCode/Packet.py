5565733a8e685ea830b47f2e6987643f3a92adcc
Anubhab Majumdar <anubhabmajumdar93@gmail.com>
2017-12-02 12:53:57 -0500

    Added ip and mac to packet

--- pythonCode/Packet.py
+++ pythonCode/Packet.py
@@ -1,6 +1,9 @@
 class Packet:
     def __init__(self, id, size=1000):
         self.id = id
+        self.ip = ''
+        self.mac = ''
+        self.hop_count = 0
         self.size = size
         self.collision_count = 0
 
